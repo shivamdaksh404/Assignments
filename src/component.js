dd322ce06a24e6acab8d93502efd282a0477f21e
shivamdaksh404 <shivofficial05@gmail.com>
2023-03-06 22:27:23 +0530

    this is assignment

---
+++ src/component.js
@@ -0,0 +1,24 @@
+export function First() {
+
+    return(
+        <h1>I am heading one ... from first component</h1>
+    );
+};
+
+export function Second () {
+    return (
+        <h2>I am heading two ... from second component</h2>
+    );
+};
+
+export function Third(){
+    return (
+        <h3> I am heading three ... from third component </h3>
+    );
+};
+
+export function Button(){
+    return (
+        <button>Click me !!</button>
+    );
+};
