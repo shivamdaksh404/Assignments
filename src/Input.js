0311b2f5c08eb1da51e9cd1e5773ffd394543ba8
shivamdaksh404 <shivofficial05@gmail.com>
2023-03-08 00:20:14 +0530

    this is input  component

---
+++ src/Input.js
@@ -0,0 +1,10 @@
+import { Fragment } from 'react' 
+export function Input() {
+    return(
+        <Fragment>
+            <input type="text" placeholder="mobile..."/>
+            <br/>
+            <input type="text" placeholder="password..."/>
+         </Fragment> 
+    );
+};
