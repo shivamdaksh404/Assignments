c1cfaf7f13e44564a7aed494792179dece8ae40f
shivamdaksh404 <shivofficial05@gmail.com>
2023-03-10 02:31:22 +0530

    this file is jsx file here I am call a component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './User Card/Card';
+
 
 function App() {
+const userOne = {
+  name:"Amit",
+  designation:"Graphic Designer",
+  description:"Highly creative and multi talented Graphic Designer with extensive experience in multimedia, marketing, and print design.",
+  image:"https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg",
+}
+const userTwo = {
+  name:"Ruhi",
+  designation:"Singer",
+  description:"perform a variety of music for recordings and live audiences. They audition for positions in choruses, orchestras, bands, plays, 				and other types of music groups.",
+  image:"https://images.pexels.com/photos/3775131/pexels-photo-3775131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <Card
+      image ={userOne.image}
+      name={userOne.name} 
+      description={userOne.description}
+      designation={userOne.designation}
+      />
+      </div>
+      <div>
+      <Card
+      image ={userTwo.image}
+      name={userTwo.name} 
+      description={userTwo.description}
+      designation={userTwo.designation}
+      />
+       </div>
     </div>
+    
   );
 }
 
